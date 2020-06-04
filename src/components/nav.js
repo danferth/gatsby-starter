3dda151e525dd65031201158f257db9d0f9fbbed
danferth <danferth@gmail.com>
2020-06-04 12:39:00 -0700

    fontawesome added of course

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome } from "@fortawesome/free-solid-svg-icons"
 
 const Nav = () => {
   return (
     <nav className="nav">
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <FontAwesomeIcon icon={faHome}></FontAwesomeIcon> Home
+          </Link>
         </li>
       </ul>
     </nav>
