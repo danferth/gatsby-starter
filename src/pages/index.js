b02475779d04257d393aa53f2e3dbf85ca74b9fd
danferth <danferth@gmail.com>
2020-06-03 17:04:01 -0700

    initial setup stqrted

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const App = () => {
+  const headercss = "uppercase text-orange-500"
+  return (
+    <div className="p-4">
+      <h1 className={headercss}>Test of tailwindcss</h1>
+      <h2 className="subheading">Oh MY!</h2>
+    </div>
+  )
 }
+
+export default App
