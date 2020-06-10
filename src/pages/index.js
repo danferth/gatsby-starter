7edf5e29a00523321b98b44a91794242d0d5422c
danferth <danferth@gmail.com>
2020-06-10 09:31:02 -0700

    added pagelink component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import Icon from "../images/svg/akio.svg"
 import { css } from "@emotion/core"
 import tw from "tailwind.macro"
+import PageLink from "../components/pagelink"
 
 const App = () => {
   return (
@@ -13,6 +14,11 @@ const App = () => {
           margin: 2rem auto;
         `}
       />
+      <p>
+        This sentance has a <PageLink to="/about">PageLink</PageLink> to the
+        about page. There is also an SVG on this page as well. Have fun and
+        enjoy!
+      </p>
     </Layout>
   )
 }
